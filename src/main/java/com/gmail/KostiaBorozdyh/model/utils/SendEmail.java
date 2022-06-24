8e0b34c9a0a0364ae68ace38a869f373676b630d
Kostia Borozdyh <kostiaborozdyh@gmail.com>
2022-06-24 15:18:37 +0300

    add send email method

--- src/main/java/com/gmail/KostiaBorozdyh/model/utils/SendEmail.java
+++ src/main/java/com/gmail/KostiaBorozdyh/model/utils/SendEmail.java
@@ -3,12 +3,14 @@ package com.gmail.KostiaBorozdyh.model.utils;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.io.UnsupportedEncodingException;
 import java.util.Properties;
+/**
+ * Util for sending email to user, using UserEmail and array of message
+ */
 
 public class SendEmail {
     public static void send(String email, String[] message)
-    {/*
+    {
         //Set Mail properties
         Properties props = System.getProperties();
         props.setProperty("mail.smtp.starttls.enable", "true");
@@ -20,23 +22,27 @@ public class SendEmail {
         Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("manager.delivery.holder@gmail.com", "managerholder");
+                return new PasswordAuthentication("manager.delivery.holder@gmail.com", "spayifhyzvspgsjh");
             }
         });
 
         //Create the email with variable input
         MimeMessage mimeMessage = new MimeMessage(session);
-        mimeMessage.setHeader("Content-Type", "text/plain; charset=UTF-8");
-        mimeMessage.setFrom(new InternetAddress("manager.delivery.holder@gmail.com", "manager delivery holder"));
-        mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
-        mimeMessage.setSubject(message[0], "utf-8");
-        mimeMessage.setContent(message[1], "text/plain; charset=UTF-8");
+        try {
+            mimeMessage.setHeader("Content-Type", "text/plain; charset=UTF-8");
+            mimeMessage.setFrom(new InternetAddress("manager.delivery.holder@gmail.com", "manager delivery holder"));
+            mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(email));
+            mimeMessage.setSubject(message[0], "utf-8");
+            mimeMessage.setContent(message[1], "text/plain; charset=UTF-8");
+            //Send the email
+            Transport.send(mimeMessage);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-        //Send the email
-        Transport.send(mimeMessage);*/
 
-        System.out.println("Email to: "+email);
+       /* System.out.println("Email to: "+email);
         System.out.println("Subject: "+message[0]);
-        System.out.println("Content: "+message[1]);
+        System.out.println("Content: "+message[1]);*/
     }
 }
